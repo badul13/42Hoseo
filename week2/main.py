aed345a80e23c132250fa752422713ec0afa0abb
badul13 <badul13@naver.com>
2025-03-11 15:13:12 +0900

    bonus challenge

--- week2/main.py
+++ week2/main.py
@@ -4,6 +4,7 @@ import os
 import errno
 
 file_path = "/42Hoseo/week2/mission_computer_main.log"
+error_file_path = "/42Hoseo/week2/error_log.txt"
 
 try:
     # 경로가 디렉터리인지 확인
@@ -13,9 +14,24 @@ try:
     # 파일 열기
     with open(file_path, "rt", encoding="utf-8") as f:
         lines = f.readlines()
+        
+        # 기존 순서대로 출력
+        print("\n=== 기존 순서 ===")
         for line in lines:
             print(line, end='')
-    f.close
+        
+        # 시간의 역순으로 정렬하여 출력
+        print("\n=== 시간 역순 ===")
+        for line in reversed(lines):
+            print(line, end='')
+        
+        # 오류 로그 저장 - explosion 검출
+        error_lines = [line for line in lines if "explosion" in line]
+        if error_lines:
+            with open(error_file_path, "wt", encoding="utf-8") as error_file:
+                error_file.writelines(error_lines)
+    
+    f.close()
 except FileNotFoundError:
     print("Error: 파일을 찾을 수 없습니다.")
 except IsADirectoryError:
